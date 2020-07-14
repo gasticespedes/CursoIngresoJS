b5c28345ee9e62785cfdc51fea9a707b87a351d5
gaston <gaston.cespedes1998@gmail.com>
2020-07-14 12:16:49 -0300

    actualizacion de curso de ingreso

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,12 +3,21 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var dato;
+
+	dato = prompt("ingrese un dato");
+	alert (dato);
 	
-	var nombre; // undefined = nunca tuvo valor 
-	nombre = "Gasti"; // string
-	nombre = null; // null = le quita el valor 
-	nombre = 12; // number
-	alert(nombre);
+
+
+	// var nombre; // undefined = nunca tuvo valor 
+	// var nombreCompleto;
+	// nombre = "Gasti"; // string
+	// nombre = null; // null = le quita el valor 
+	// nombre = 12; // number
+	// nombre = true; // bolean
+
+	// alert(nombre);
 	
 
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,8 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var dato 
+	dato = prompt("dato");
+	txtIdNombre.value = dato
 }
 
